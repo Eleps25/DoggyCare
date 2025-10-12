ff9090461b56ca8a1572eccfce82600dea3ddbce
Eleps25 <mezuja@gmail.com>
2025-10-12 22:31:32 +0200

    DatabaseHelper - comment on future functions

--- DoggyCare/Helpers/DatabaseHelper.cs
+++ DoggyCare/Helpers/DatabaseHelper.cs
@@ -1,5 +1,15 @@
 ﻿namespace DoggyCare.Helpers {
     internal class DatabaseHelper {
+        // Check or Init DB
 
+        // Get Records
+
+        // Get Record with ID
+
+        // Add Record
+
+        // Update Record
+
+        // Delete Record
     }
 }
