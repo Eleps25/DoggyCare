f81d480825464df120b13c9a02a04e8174b3efe0
Eleps25 <mezuja@gmail.com>
2025-10-12 22:14:09 +0200

    Base CareRecord Layout

--- DoggyCare/Models/CareRecord.cs
+++ DoggyCare/Models/CareRecord.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DoggyCare.Enums;
 
 namespace DoggyCare.Models {
-    internal class CareRecord {
+    public class CareRecord {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public CareRecordType Type { get; set; }
+        public decimal Price { get; set; }
+        public decimal? Weight { get; set; }
+        public string Description { get; set; }
     }
 }
