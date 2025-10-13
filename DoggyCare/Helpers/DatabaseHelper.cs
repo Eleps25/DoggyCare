6a4fc1ad4ddda4934b86bc3491a6bfddb1bf6566
Eleps25 <mezuja@gmail.com>
2025-10-13 19:44:41 +0200

    Check and Init DB

--- DoggyCare/Helpers/DatabaseHelper.cs
+++ DoggyCare/Helpers/DatabaseHelper.cs
@@ -1,7 +1,33 @@
-﻿namespace DoggyCare.Helpers {
-    internal class DatabaseHelper {
-        // Check or Init DB
+﻿using System.Configuration;
+using System.Data.SQLite;
 
+namespace DoggyCare.Helpers {
+    public static class DatabaseHelper {
+        private static string connectionString = ConfigurationManager.AppSettings.Get("connectString");
+        private static string tableName = ConfigurationManager.AppSettings.Get("tableName");
+        // Check or Init DB
+        public static void CheckOrInitDb() {
+            using (var connection = new SQLiteConnection(connectionString)) {
+                connection.Open();
+                var cmd = connection.CreateCommand();
+
+                cmd.CommandText =
+                    $@"CREATE TABLE IF NOT EXISTS {tableName}(
+                                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                Date TEXT NOT NULL,
+                                Year INTEGER NOT NULL,
+                                Month INTEGER NOT NULL,
+                                Day INTEGER NOT NULL,
+                                Type INTEGER NOT NULL,
+                                Price REAL NOT NULL,
+                                Weight REAL,
+                                Description TEXT
+                                );";
+                cmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
         // Get Records
 
         // Get Record with ID
--- DoggyCare/Models/CareRecord.cs
+++ DoggyCare/Models/CareRecord.cs
@@ -4,6 +4,9 @@ namespace DoggyCare.Models {
     public class CareRecord {
         public int Id { get; set; }
         public DateTime Date { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
         public CareRecordType Type { get; set; }
         public decimal Price { get; set; }
         public decimal? Weight { get; set; }
