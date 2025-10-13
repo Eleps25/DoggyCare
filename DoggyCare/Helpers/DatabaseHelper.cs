914466f53676821c30e78b67268e626afd5b6c9f
Eleps25 <mezuja@gmail.com>
2025-10-13 23:18:25 +0200

    Úpravy DB funkcí

--- DoggyCare/Helpers/DatabaseHelper.cs
+++ DoggyCare/Helpers/DatabaseHelper.cs
@@ -82,7 +82,7 @@ namespace DoggyCare.Helpers {
                 cmd.Parameters.AddWithValue("@Year", inputCareRecord.Year);
                 cmd.Parameters.AddWithValue("@Month", inputCareRecord.Month);
                 cmd.Parameters.AddWithValue("@Day", inputCareRecord.Day);
-                cmd.Parameters.AddWithValue("@Type", inputCareRecord.Type);
+                cmd.Parameters.AddWithValue("@Type", (int)inputCareRecord.Type);
                 cmd.Parameters.AddWithValue("@Price", (double)inputCareRecord.Price);
                 if (inputCareRecord.Weight.HasValue) {
                     cmd.Parameters.AddWithValue("@Weight", (double)inputCareRecord.Weight.Value);
@@ -100,12 +100,12 @@ namespace DoggyCare.Helpers {
             using (var connection = new SQLiteConnection(connectionString)) {
                 connection.Open();
                 var cmd = connection.CreateCommand();
-                cmd.CommandText = $"UPDATE {tableName} (Date, Year, Month, Day, Type, Price, Weight, Description) VALUES(@Date, @Year, @Month, @Day, @Type, @Price, @Weight, @Description)";
+                cmd.CommandText = $"UPDATE {tableName} SET Date = @Date, Year = @Year, Month = @Month, Day = @Day, Type = @Type, Price = @Price, Weight = @Weight, Description = @Description";
                 cmd.Parameters.AddWithValue("@Date", updateCareRecord.Date.ToString("yyyy-MM-dd"));
                 cmd.Parameters.AddWithValue("@Year", updateCareRecord.Year);
                 cmd.Parameters.AddWithValue("@Month", updateCareRecord.Month);
                 cmd.Parameters.AddWithValue("@Day", updateCareRecord.Day);
-                cmd.Parameters.AddWithValue("@Type", updateCareRecord.Type);
+                cmd.Parameters.AddWithValue("@Type", (int)updateCareRecord.Type);
                 cmd.Parameters.AddWithValue("@Price", (double)updateCareRecord.Price);
                 if (updateCareRecord.Weight.HasValue) {
                     cmd.Parameters.AddWithValue("@Weight", (double)updateCareRecord.Weight.Value);
