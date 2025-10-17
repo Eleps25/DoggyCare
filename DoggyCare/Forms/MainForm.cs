697df3cc1c09a6ce2350a2c1a3b6eeae03c1b0d5
Eleps25 <mezuja@gmail.com>
2025-10-17 11:30:39 +0200

    Page Navigation + Page Intergace and Type

--- DoggyCare/Forms/MainForm.cs
+++ DoggyCare/Forms/MainForm.cs
@@ -1,3 +1,8 @@
+using DoggyCare.Enums;
+using DoggyCare.Extensions;
+using DoggyCare.Interfaces;
+using DoggyCare.Navigation;
+
 namespace DoggyCare {
     public partial class MainForm : Form {
         public MainForm() {
@@ -10,9 +15,23 @@ namespace DoggyCare {
         }
 
         private void btnDashboard_Click(object sender, EventArgs e) {
+            ShowPage(PageType.Dashboard);
         }
 
         private void btnRecords_Click(object sender, EventArgs e) {
+            ShowPage(PageType.CareRecords);
+        }
+
+        private void ShowPage(PageType pageType) {
+            IPage page = PageManager.GetPage(pageType);
+
+            mainPanel.Controls.Clear();
+
+            var view = page.GetView();
+            view.Dock = DockStyle.Fill;
+            mainPanel.Controls.Add(view);
+
+            lblTitle.Text = EnumExtensions.GetEnumDescription(pageType);
         }
     }
 }
---
+++ DoggyCare/Navigation/PageManager.cs
@@ -0,0 +1,18 @@
+﻿using DoggyCare.Enums;
+using DoggyCare.Interfaces;
+using DoggyCare.Pages;
+
+namespace DoggyCare.Navigation {
+    internal class PageManager {
+        public static IPage GetPage(PageType pageType) {
+            switch (pageType) {
+                case PageType.Dashboard:
+                    return new DashboardPage();
+                case PageType.CareRecords:
+                    return new CareRecordsPage();
+                default:
+                    return new DashboardPage(); // fallback
+            }
+        }
+    }
+}
