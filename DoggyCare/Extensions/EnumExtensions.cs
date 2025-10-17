6caae01a8c57d6f67104e5e5f10a4040691bc331
Eleps25 <mezuja@gmail.com>
2025-10-17 11:31:12 +0200

    Enum Extension for displaying enums descriptions

---
+++ DoggyCare/Extensions/EnumExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace DoggyCare.Extensions {
+    internal class EnumExtensions {
+        public static string GetEnumDescription(Enum value) {
+            FieldInfo field = value.GetType().GetField(value.ToString());
+            DescriptionAttribute attr = field.GetCustomAttribute<DescriptionAttribute>();
+            return attr?.Description ?? value.ToString();
+        }
+    }
+}
