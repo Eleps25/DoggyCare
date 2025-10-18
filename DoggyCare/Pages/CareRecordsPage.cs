3ecb96564446b9e0e45bce2e5229e129dcd93d87
Eleps25 <mezuja@gmail.com>
2025-10-18 12:31:54 +0200

    CareRecordsPage refactor

--- DoggyCare/Pages/CareRecordsPage.cs
+++ DoggyCare/Pages/CareRecordsPage.cs
@@ -2,14 +2,13 @@
 using DoggyCare.Extensions;
 using DoggyCare.Helpers;
 using DoggyCare.Interfaces;
-using DoggyCare.Models;
 using System.Data;
 
 namespace DoggyCare.Pages {
     public partial class CareRecordsPage : UserControl, IPage {
         public CareRecordsPage() {
             InitializeComponent();
-            StylizeDataGridView(dgvCareRecords);
+            CareRecordsHelperFunctions.StylizeDataGridView(dgvCareRecords);
             UpdateCareRecords();
             //this.BackColor = Color.Orange;
             //Label lbl = new Label() { Text = "Dashboard", Font = new Font("Segoe UI", 14), AutoSize = true };
@@ -17,35 +16,7 @@ namespace DoggyCare.Pages {
         }
 
         private void UpdateCareRecords() {
-            DataTable dataTable = new DataTable();
-
-            DataColumn DateCol = dataTable.Columns.Add("Date", typeof(DateTime));
-            DataColumn typeCol = dataTable.Columns.Add("Type", typeof(CareRecordType));
-            DataColumn priceCol = dataTable.Columns.Add("Price", typeof(decimal));
-            DataColumn weightCol = dataTable.Columns.Add("Weight");
-            DataColumn descriptionCol = dataTable.Columns.Add("Description", typeof(string));
-
-            DateCol.Caption = "Datum";
-            typeCol.Caption = "Typ";
-            priceCol.Caption = "Cena (Kč)";
-            weightCol.Caption = "Váha (Kg)";
-            descriptionCol.Caption = "Popis";
-
-            // Vyplnit data
-            List<CareRecord> careRecords = DatabaseHelper.GetCareRecords();
-
-            foreach (CareRecord careRecord in careRecords) {
-                var row = dataTable.NewRow();
-
-                row["Date"] = careRecord.Date;
-                row["Type"] = careRecord.Type;
-                row["Price"] = careRecord.Price;
-                row["Weight"] = careRecord.Weight;
-                row["Description"] = careRecord.Description;
-
-                dataTable.Rows.Add(row);
-            }
-
+            DataTable dataTable = CareRecordsHelperFunctions.GetDataTableCareRecords();
             this.dgvCareRecords.DataSource = dataTable;
 
             // Změnit zobrazení českým popisem
@@ -55,31 +26,6 @@ namespace DoggyCare.Pages {
         }
 
         public UserControl GetView() => this;
-
-        private void StylizeDataGridView(DataGridView dataGridView) {
-            // DGV Styles
-            dataGridView.BorderStyle = BorderStyle.None;
-            dataGridView.CellBorderStyle = DataGridViewCellBorderStyle.None;
-            dataGridView.GridColor = Color.FromArgb(233, 236, 239); // #E9ECEF
-            dataGridView.BackgroundColor = Color.White;
-            dataGridView.EnableHeadersVisualStyles = false;
-
-            // Header styles
-            dataGridView.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
-            dataGridView.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(241, 243, 245); // #F1F3F5
-            dataGridView.ColumnHeadersDefaultCellStyle.ForeColor = Color.FromArgb(47, 62, 70);   // #2F3E46
-            dataGridView.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 10, FontStyle.Bold);
-            dataGridView.ColumnHeadersDefaultCellStyle.SelectionBackColor = Color.FromArgb(178, 223, 219); // #B2DFDB
-
-            // Cell styles
-            dataGridView.DefaultCellStyle.BackColor = Color.White;
-            dataGridView.DefaultCellStyle.ForeColor = Color.FromArgb(51, 51, 51);
-            dataGridView.DefaultCellStyle.SelectionBackColor = Color.FromArgb(224, 247, 244); // #E0F7F4
-            dataGridView.DefaultCellStyle.SelectionForeColor = Color.FromArgb(51, 51, 51);
-            dataGridView.DefaultCellStyle.Font = new Font("Segoe UI", 10, FontStyle.Regular);
-
-            dataGridView.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(250, 250, 250); // střídání řádků
-        }
 
         private void dgvCareRecords_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e) {
             if (dgvCareRecords.Columns[e.ColumnIndex].DataPropertyName == "Type") {
